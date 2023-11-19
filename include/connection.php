cf2388b1186091d12f06f2b0c16b36944df537ef
Mark Piolo Pascual <piolopascual410@gmail.com>
2023-11-19 15:40:30 +0800

    Updated Connection

--- include/connection.php
+++ include/connection.php
@@ -1,6 +1,12 @@
 <?php
 
-$dbConn = mysqli_connect ("localhost", "root", "") or die ('MySQL connect failed. ' );
-mysqli_select_db($dbConn, "easecholar") or die('Cannot select database. '  );
+$host = "easecholar-do-user-15025878-0.c.db.ondigitalocean.com";
+$port = 25060;
+$username = "doadmin";
+$password = "AVNS_I9q9-Dls3cwYbepb0qX";
+$database = "defaultdb";
+$sslmode = "REQUIRED";
+
+$dbConn = mysqli_connect($host, $username, $password, $database, $port, null) or die('MySQL connect failed: ' . mysqli_connect_error());
 
 ?>

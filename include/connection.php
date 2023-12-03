bda9d41c3e8fb65986b311afe6dbc95dbf9d1c3d
Mark Piolo Pascual <piolopascual410@gmail.com>
2023-12-03 19:11:50 +0800

    Connection MODIFIED

--- include/connection.php
+++ include/connection.php
@@ -4,10 +4,8 @@ $host = "ease-cholar-server.mysql.database.azure.com";
 $username = "httzpwmikk";
 $password = "XTPPY2C81FS5328B$";
 $database = "easecholar";
-$sslmode = "require"; // Note: "require" instead of "REQUIRED"
 
 $dbConn = mysqli_init();
-mysqli_ssl_set($dbConn, NULL, NULL, NULL, NULL, NULL);
-mysqli_real_connect($dbConn, $host, $username, $password, $database, 3306, NULL, MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT) or die('MySQL connect failed: ' . mysqli_connect_error());
+mysqli_real_connect($dbConn, $host, $username, $password, $database, 3306, NULL, 0) or die('MySQL connect failed: ' . mysqli_connect_error());
 
 ?>

8a9211483f4b50f616924307b1a2f4e62f5f5201
Mark Piolo Pascual <piolopascual410@gmail.com>
2023-12-03 19:02:04 +0800

    Connection modified

--- include/connection.php
+++ include/connection.php
@@ -1,12 +1,13 @@
 <?php
 
-$host = "easecholar-do-user-15025878-0.c.db.ondigitalocean.com";
-$port = 25060;
-$username = "doadmin";
-$password = "AVNS_I9q9-Dls3cwYbepb0qX";
-$database = "defaultdb";
-$sslmode = "REQUIRED";
+$host = "ease-cholar-server.mysql.database.azure.com";
+$username = "httzpwmikk";
+$password = "XTPPY2C81FS5328B$";
+$database = "easecholar";
+$sslmode = "require"; // Note: "require" instead of "REQUIRED"
 
-$dbConn = mysqli_connect($host, $username, $password, $database, $port, null) or die('MySQL connect failed: ' . mysqli_connect_error());
+$dbConn = mysqli_init();
+mysqli_ssl_set($dbConn, NULL, NULL, NULL, NULL, NULL);
+mysqli_real_connect($dbConn, $host, $username, $password, $database, 3306, NULL, MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT) or die('MySQL connect failed: ' . mysqli_connect_error());
 
 ?>

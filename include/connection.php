6250ab7b52db36abe8b944df2ed0d4813eb3b30b
Mark Piolo Pascual <piolopascual410@gmail.com>
2023-12-03 19:20:41 +0800

    Modified the Connection

--- include/connection.php
+++ include/connection.php
@@ -1,11 +1,27 @@
 <?php
-
-$host = "ease-cholar-server.mysql.database.azure.com";
-$username = "httzpwmikk";
+$serverName = "ease-cholar-server.mysql.database.azure.com";
+$databaseName = "httzpwmikk";
+$username = "easecholar";
 $password = "XTPPY2C81FS5328B$";
-$database = "easecholar";
 
+// Check if the mysqli extension is available
+if (!function_exists('mysqli_init')) {
+    die('mysqli extension is not available. Please enable it in your PHP configuration.');
+}
+
+// Construct the connection string with SSL options
+$connectionString = "Server=$serverName;Database=$databaseName;User Id=$username;Password=$password;Encrypt=true;TrustServerCertificate=false";
+
+// Create a database connection with the mysqli_init function
 $dbConn = mysqli_init();
-mysqli_real_connect($dbConn, $host, $username, $password, $database, 3306, NULL, 0) or die('MySQL connect failed: ' . mysqli_connect_error());
+if (!$dbConn) {
+    die("mysqli_init failed");
+}
 
+// Connect to the database using mysqli_real_connect with SSL
+if (!$dbConn->real_connect($serverName, $username, $password, $databaseName, 3306, NULL, MYSQLI_CLIENT_SSL)) {
+    die("Connect Error: " . mysqli_connect_error());
+}
+
+// Now $conn is your mysqli connection object
 ?>
